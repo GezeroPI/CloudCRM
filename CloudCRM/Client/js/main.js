b844ba44f9b4c96a43a36f599840b826def480ca
GezeroPI <info@ndh-webstudio.com>
2018-05-18 14:20:20 +0300

    Webpack working

--- CloudCRM/Client/js/main.js
+++
@@ -1,8 +0,0 @@
-﻿import '../css/site.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'jquery';
-import 'bootstrap';
-import 'font-awesome/css/font-awesome.css';
-import 'chart.js';
-import 'jquery.easing';
-import 'datatables.net-bs4';
---
+++ CloudCRM/src/main.js
@@ -0,0 +1,11 @@
+﻿import './css/site.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery';
+import 'font-awesome/css/font-awesome.css';
+import 'chart.js';
+import 'jquery.easing';
+import 'datatables.net-bs4';
+import './js/sb-admin.min.js';
+
+
